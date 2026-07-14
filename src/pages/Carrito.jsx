f1de24fc0c447189385cab3f04d00c3bf0ffb7d7
Neo Neo <jesusggxbk@gmail.com>
2026-07-14 07:05:20 -0600

    correccion en carrito vacio

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -18,7 +18,7 @@ function Carrito() {
         <h1 className="text-2xl font-bold mb-4">🛒 Carrito de compras</h1>
         <p className="text-gray-500">Tu carrito está vacío.</p>
         <button
-          onClick={() => navigate('/productos')}
+          onClick={() => navigate('/home')}
           className="mt-4 bg-indigo-600 text-white px-6 py-2 rounded-lg hover:bg-indigo-700"
         >
           Ver productos
