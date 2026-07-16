2a32b7de78a70897111c5ae28f3b91783a94cf93
Neo Neo <jesusggxbk@gmail.com>
2026-07-16 11:53:36 -0600

    actualiza navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -34,14 +34,6 @@ function Navbar() {
                     Productos
                 </Link>
 
-                <Link to="/carrito" className="relative">
-                    🛒
-                    {cantidadTotal > 0 && (
-                        <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">
-                            {cantidadTotal}
-                        </span>
-                    )}
-                </Link>
 
                 <span>
                     👤 {username}
