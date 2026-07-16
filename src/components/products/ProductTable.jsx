bc828f17c1de92328fb73ad3d2686573df8635a9
Neo Neo <jesusggxbk@gmail.com>
2026-07-16 11:27:40 -0600

    ajusta vista usuario

--- src/components/products/ProductTable.jsx
+++ src/components/products/ProductTable.jsx
@@ -53,12 +53,7 @@ function ProductTable({ productos, onProductoEliminado }) {
                     Listado de Productos
                 </h2>
 
-                <button
-                    onClick={agregarProducto}
-                    className="w-full md:w-auto bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded"
-                >
-                    + Agregar Producto
-                </button>
+                
 
             </div>
 
