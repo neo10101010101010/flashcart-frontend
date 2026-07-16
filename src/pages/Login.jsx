4b495a9c035a4d5c170826cba7eefc52d405eb01
Neo Neo <jesusggxbk@gmail.com>
2026-07-16 06:59:48 -0600

    añade link

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect} from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import { login } from "../services/authService";
 import useAuthStore from "../store/authStore";
